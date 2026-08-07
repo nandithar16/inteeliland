32fe5e33cfcbff8486413d354843a38c20b41cb0
GitHub Pages <actions@github.com>
2026-08-08 04:42:14 +0530

    Fix GitHub Pages base path for deployed site

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: "/Intel-Land/",
+  base: "/Inteeliland/",
   server: {
     host: "::",
     port: 8080,
